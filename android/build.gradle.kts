dc331021ae79298a1073fd86deb22e5c1363609b
yudaprama <yudhaprama@icloud.com>
2026-03-20 03:48:09 +0700

    Update google_mlkit dependencies and fix namespace issue
    - Updated `google_mlkit_commons` to version 0.11.1 and `google_mlkit_text_recognition` to version 0.15.1 in `pubspec.yaml` and `pubspec.lock`.
    - Added a fix in `build.gradle.kts` to address the namespace issue for `google_mlkit_commons`, ensuring proper configuration for the library.
    
    This update enhances the app's functionality by incorporating the latest versions of the ML Kit libraries and resolving namespace conflicts.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,14 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Fix for google_mlkit_commons namespace issue
+    if (project.name == "google_mlkit_commons") {
+        project.apply(plugin = "com.android.library")
+        project.extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
+            namespace = "com.google.mlkit.common"
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
