63e243537c97a29722ae51cb658baf175c262938
yudaprama <yudhaprama@icloud.com>
2026-03-20 19:36:36 +0700

    Enable debug logging for Sentry in main.dart
    - Added debug mode and diagnostic level settings for Sentry to enhance error tracking and logging capabilities.
    - This update improves the visibility of Sentry logs, aiding in the debugging process during development.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ Future<void> main() async {
       // Adds request headers and IP for users, for more info visit:
       // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
       options.sendDefaultPii = true;
+      options.debug = true;
+      options.diagnosticLevel = SentryLevel.debug;
       options.enableLogs = true;
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
       // We recommend adjusting this value in production.
